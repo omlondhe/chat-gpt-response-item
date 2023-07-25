e035f64a28cf57d35c3cb25fdb9d95edc5b0eeb7
= <=>
2023-07-25 15:40:57 +0530

    Completed with basic working setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,98 @@
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import { useEffect, useState } from 'react';
+import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
+import {PrismLight as SyntaxHighlighter} from 'react-syntax-highlighter'
+import {materialLight} from 'react-syntax-highlighter/dist/cjs/styles/prism'
+import CopyToClipboard from 'react-copy-to-clipboard';
+import jsx from 'react-syntax-highlighter/dist/cjs/languages/prism/jsx'
+import { ClipboardIcon } from '@heroicons/react/24/outline'
 
 const inter = Inter({ subsets: ['latin'] })
 
+const javaScriptMarkdownCodeblock = `**JavaScript** _code_ example:
+ 
+~~~js
+// function that adds "2 numbers" together
+const sumTwoNumbers = (num1, num2) => num1 + num2;
+ 
+// call the function
+console.log(sumTwoNumbers(1, 2)); // 3
+ 
+
+// array of users
+const users = [
+  { name: "John", age: 30 },
+  { name: "Jane", age: 28 },
+  { name: "Bob", age: 25 },
+];
+ 
+// print out the users age 
+console.log(users.map(user => user.age)); // [30, 28, 25]
+~~~
+
+Time complexity: \`O(N)\`
+
+## Hello
+
+Hi
+`;
+
 export default function Home() {
+  const [postContent, setPostContent] = useState("")
+  const [isCopied, setIsCopied] = useState(false)
+
+  SyntaxHighlighter.registerLanguage('jsx', jsx);
+
+  const setCopied = () => {
+    setIsCopied(true)
+    setTimeout(() => { setIsCopied(false) }, 1000);
+  }
+
+  useEffect(() => {
+    let index = 0;
+    let response = "";
+    const array = javaScriptMarkdownCodeblock.split(" ");
+    let interval = setInterval(() => {
+      if (index < array.length) {
+        response += ` ${array[index++]}`;
+        setPostContent(response);
+      }
+      else clearInterval(interval);
+    }, (Math.random() + 5) * 10);
+  }, [])
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <ReactMarkdown
+      components={{
+        code({node, inline, className, children, ...props}) {
+          const match = /language-(\w+)/.exec(className || '')
+          const data = String(children).replace(/\n$/, '');
+          return !inline && match ? (
+            <div className="relative">
+              <div className="absolute top-1 right-1 z-50 w-fit">
+                <CopyToClipboard text={data}>
+                  <button onClick={() => setCopied()} className='w-fit bg-transparent border-none py-5 px-10 cursor-pointer text-sm outline-none border-1 border-black rounded-md'>
+                    <ClipboardIcon className={`h-6 w-6 ${isCopied ? 'text-white' : 'text-gray-500'} ${isCopied ? 'bg-indigo-600' : 'bg-transparent'} rounded-md p-1`} />
+                  </button>
+                </CopyToClipboard>
+              </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+              <SyntaxHighlighter
+                {...props}
+                style={materialLight}
+                language={match[1]}
+                PreTag="div"
+                codeTagProps={{ style: { background: 'transparent', fontSize: 'small' } }} showLineNumbers lineNumberStyle={{ background: 'transparent', borderRight: '1px solid', marginRight: 20 }} customStyle={{ backgroundColor: 'transparent' }}
+                >{data}</SyntaxHighlighter>
+            </div>
+          ) : (
+            <code {...props} className={className}>
+              {children}
+            </code>
+          )
+        }
+      }}>
+        {postContent}
+      </ReactMarkdown>
   )
 }
